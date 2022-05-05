7aa414947ea401940b6bcf5b852b27ced0f8dfb7
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-05-06 00:04:10 +0430

    writeStream

--- learns/streams.js
+++ learns/streams.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 
 const readStream = fs.createReadStream('./learns/docs/blog3.txt', { encoding: 'utf8' });
+const writeStream = fs.createWriteStream('./learns/docs/blog4.txt');
 
 readStream.on('data', (chunk) => {
     console.log('----- New -----');
     console.log(chunk);
+    writeStream.write('\nNew Chunk\n');
+    writeStream.write(chunk);
 });
