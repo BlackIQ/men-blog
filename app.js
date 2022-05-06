f54c5f8fa74cef1e1dfbb105743a7d933a7d5e94
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-05-06 11:54:02 +0430

    Render :))))))))))))

--- app.js
+++ app.js
@@ -3,16 +3,15 @@ const express = require('express');
 // Express App
 const app = express();
 
+app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    // res.send('Hello Home!');
-    res.sendFile('./views/index.html', {root: __dirname});
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    // res.send('Hello About!');
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about');
 });
 
 app.get('/panel', (req, res) => {
@@ -20,5 +19,5 @@ app.get('/panel', (req, res) => {
 });
 
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404');
 })
