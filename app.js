1ac163127a49bc28c306b15fc17ca90486e5aca7
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-05-06 15:29:06 +0430

    Added add sample stuff

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const Blog = require('./modules/blog');
 // Express App
 const app = express();
 
-const dbURI = "mongodb+srv://ninja:test1234@menblogs.ji4jf.mongodb.net/blogs?retryWrites=true&w=majority";
+const dbURI = "mongodb+srv://ninja:test1234@menblogs.ji4jf.mongodb.net/menblogs?retryWrites=true&w=majority";
 mongoose.connect(dbURI)
     .then((result) => {
         console.log('Connected!');
@@ -20,42 +20,6 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 app.use(morgan('dev'));
-
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: 'Title',
-        snippet: 'First',
-        body: 'Hello Gangs!'
-    });
-
-    blog.save()
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((error) => {
-            res.send(error);
-        });
-});
-
-app.get('/get-blogs', (req, res) => {
-    Blog.find()
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((error) => {
-            res.send(error);
-        });
-});
-
-app.get('/get-single', (req, res) => {
-    Blog.findById('6274fa45375c1673d3560d53')
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((error) => {
-            res.send(error);
-        })
-});
 
 app.get('/', (req, res) => {
     res.redirect('/blogs');
